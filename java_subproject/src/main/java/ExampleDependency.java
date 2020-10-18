7891348ed157be6a4f975a04022ac213cac806ed
Shyam Reyal <shyam3001@gmail.com>
2020-10-18 09:40:38 +0530

    added external dependency apache commons CSV

--- java_subproject/src/main/java/ExampleDependency.java
+++ java_subproject/src/main/java/ExampleDependency.java
@@ -1,7 +1,18 @@
 package org.example.dependency;
 
+import org.apache.commons.csv.*;
+import java.io.*;
+import java.time.*;
+
 public class ExampleDependency {
     public static void exampleMethod() {
-        System.out.println("Hello Dependency");
+        try (CSVPrinter printer = new CSVPrinter(new FileWriter("csv.txt"), CSVFormat.EXCEL)) {
+		     printer.printRecord("id", "userName", "firstName", "lastName", "birthday");
+		     printer.printRecord(1, "john73", "John", "Doe", LocalDate.of(1973, 9, 15));
+		     printer.println();
+		     printer.printRecord(2, "mary", "Mary", "Meyer", LocalDate.of(1985, 3, 29));
+		 } catch (IOException ex) {
+		     ex.printStackTrace();
+		 }
     }
 }
